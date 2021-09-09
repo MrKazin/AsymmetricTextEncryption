b46b120aed151d82420002a20770ae6c752636cd
MrKaz <MrKaz@DESKTOP-FK7J07S>
2021-09-09 15:51:10 +0400

    Add project files.

---
+++ MySolution/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace MySolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AdditionalInfo = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveParamsButton = new System.Windows.Forms.Button();
+            this.generateRSAButton = new System.Windows.Forms.Button();
+            this.PlainText = new System.Windows.Forms.TextBox();
+            this.textGroup = new System.Windows.Forms.GroupBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.loadPublicKey = new System.Windows.Forms.Button();
+            this.EncryptText = new System.Windows.Forms.Button();
+            this.encryptedTextGroup = new System.Windows.Forms.GroupBox();
+            this.CipheredText = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CipheredBytes = new System.Windows.Forms.TextBox();
+            this.SaveCipheredText = new System.Windows.Forms.Button();
+            this.LoadBothKeys = new System.Windows.Forms.Button();
+            this.DecryptFile = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.textGroup.SuspendLayout();
+            this.encryptedTextGroup.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AdditionalInfo
+            // 
+            this.AdditionalInfo.Location = new System.Drawing.Point(6, 21);
+            this.AdditionalInfo.Multiline = true;
+            this.AdditionalInfo.Name = "AdditionalInfo";
+            this.AdditionalInfo.ReadOnly = true;
+            this.AdditionalInfo.Size = new System.Drawing.Size(971, 261);
+            this.AdditionalInfo.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.ReadOnlyChecked = true;
+            // 
+            // saveParamsButton
+            // 
+            this.saveParamsButton.Enabled = false;
+            this.saveParamsButton.Location = new System.Drawing.Point(122, 21);
+            this.saveParamsButton.Name = "saveParamsButton";
+            this.saveParamsButton.Size = new System.Drawing.Size(104, 30);
+            this.saveParamsButton.TabIndex = 3;
+            this.saveParamsButton.Text = "Save Keys";
+            this.saveParamsButton.UseVisualStyleBackColor = true;
+            this.saveParamsButton.Click += new System.EventHandler(this.saveParamsButton_Click);
+            // 
+            // generateRSAButton
+            // 
+            this.generateRSAButton.Location = new System.Drawing.Point(6, 21);
+            this.generateRSAButton.Name = "generateRSAButton";
+            this.generateRSAButton.Size = new System.Drawing.Size(110, 30);
+            this.generateRSAButton.TabIndex = 6;
+            this.generateRSAButton.Text = "Generate RSA";
+            this.generateRSAButton.UseVisualStyleBackColor = true;
+            this.generateRSAButton.Click += new System.EventHandler(this.generateRSAButton_Click);
+            // 
+            // PlainText
+            // 
+            this.PlainText.Location = new System.Drawing.Point(6, 21);
+            this.PlainText.Multiline = true;
+            this.PlainText.Name = "PlainText";
+            this.PlainText.Size = new System.Drawing.Size(971, 48);
+            this.PlainText.TabIndex = 4;
+            // 
+            // textGroup
+            // 
+            this.textGroup.Controls.Add(this.PlainText);
+            this.textGroup.Location = new System.Drawing.Point(12, 389);
+            this.textGroup.Name = "textGroup";
+            this.textGroup.Size = new System.Drawing.Size(984, 81);
+            this.textGroup.TabIndex = 7;
+            this.textGroup.TabStop = false;
+            this.textGroup.Text = "Text";
+            // 
+            // loadPublicKey
+            // 
+            this.loadPublicKey.Location = new System.Drawing.Point(6, 21);
+            this.loadPublicKey.Name = "loadPublicKey";
+            this.loadPublicKey.Size = new System.Drawing.Size(124, 30);
+            this.loadPublicKey.TabIndex = 8;
+            this.loadPublicKey.Text = "Load Public Key";
+            this.loadPublicKey.UseVisualStyleBackColor = true;
+            this.loadPublicKey.Click += new System.EventHandler(this.loadPublicKey_Click);
+            // 
+            // EncryptText
+            // 
+            this.EncryptText.Enabled = false;
+            this.EncryptText.Location = new System.Drawing.Point(136, 21);
+            this.EncryptText.Name = "EncryptText";
+            this.EncryptText.Size = new System.Drawing.Size(104, 30);
+            this.EncryptText.TabIndex = 9;
+            this.EncryptText.Text = "Encrypt Text";
+            this.EncryptText.UseVisualStyleBackColor = true;
+            this.EncryptText.Click += new System.EventHandler(this.EncryptFile_Click);
+            // 
+            // encryptedTextGroup
+            // 
+            this.encryptedTextGroup.Controls.Add(this.CipheredText);
+            this.encryptedTextGroup.Location = new System.Drawing.Point(12, 485);
+            this.encryptedTextGroup.Name = "encryptedTextGroup";
+            this.encryptedTextGroup.Size = new System.Drawing.Size(984, 81);
+            this.encryptedTextGroup.TabIndex = 8;
+            this.encryptedTextGroup.TabStop = false;
+            this.encryptedTextGroup.Text = "Ciphered Text as Text String";
+            // 
+            // CipheredText
+            // 
+            this.CipheredText.Location = new System.Drawing.Point(6, 21);
+            this.CipheredText.Multiline = true;
+            this.CipheredText.Name = "CipheredText";
+            this.CipheredText.ReadOnly = true;
+            this.CipheredText.Size = new System.Drawing.Size(971, 48);
+            this.CipheredText.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.AdditionalInfo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(984, 301);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Additional Info";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.CipheredBytes);
+            this.groupBox2.Location = new System.Drawing.Point(12, 583);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(984, 135);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ciphered Text as Byte Array";
+            // 
+            // CipheredBytes
+            // 
+            this.CipheredBytes.Location = new System.Drawing.Point(6, 21);
+            this.CipheredBytes.Multiline = true;
+            this.CipheredBytes.Name = "CipheredBytes";
+            this.CipheredBytes.ReadOnly = true;
+            this.CipheredBytes.Size = new System.Drawing.Size(971, 102);
+            this.CipheredBytes.TabIndex = 4;
+            // 
+            // SaveCipheredText
+            // 
+            this.SaveCipheredText.Enabled = false;
+            this.SaveCipheredText.Location = new System.Drawing.Point(246, 21);
+            this.SaveCipheredText.Name = "SaveCipheredText";
+            this.SaveCipheredText.Size = new System.Drawing.Size(211, 30);
+            this.SaveCipheredText.TabIndex = 11;
+            this.SaveCipheredText.Text = "Save Ciphered Text And Bytes";
+            this.SaveCipheredText.UseVisualStyleBackColor = true;
+            this.SaveCipheredText.Click += new System.EventHandler(this.SaveCipheredText_Click);
+            // 
+            // LoadBothKeys
+            // 
+            this.LoadBothKeys.Location = new System.Drawing.Point(6, 21);
+            this.LoadBothKeys.Name = "LoadBothKeys";
+            this.LoadBothKeys.Size = new System.Drawing.Size(155, 30);
+            this.LoadBothKeys.TabIndex = 12;
+            this.LoadBothKeys.Text = "Load Keys And Bytes";
+            this.LoadBothKeys.UseVisualStyleBackColor = true;
+            this.LoadBothKeys.Click += new System.EventHandler(this.LoadBothKeys_Click);
+            // 
+            // DecryptFile
+            // 
+            this.DecryptFile.Enabled = false;
+            this.DecryptFile.Location = new System.Drawing.Point(167, 21);
+            this.DecryptFile.Name = "DecryptFile";
+            this.DecryptFile.Size = new System.Drawing.Size(96, 30);
+            this.DecryptFile.TabIndex = 13;
+            this.DecryptFile.Text = "Decrypt File";
+            this.DecryptFile.UseVisualStyleBackColor = true;
+            this.DecryptFile.Click += new System.EventHandler(this.DecryptFile_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.generateRSAButton);
+            this.groupBox3.Controls.Add(this.saveParamsButton);
+            this.groupBox3.Location = new System.Drawing.Point(18, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(232, 64);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Generate";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.loadPublicKey);
+            this.groupBox4.Controls.Add(this.EncryptText);
+            this.groupBox4.Controls.Add(this.SaveCipheredText);
+            this.groupBox4.Location = new System.Drawing.Point(256, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(464, 64);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Encrypt";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.LoadBothKeys);
+            this.groupBox5.Controls.Add(this.DecryptFile);
+            this.groupBox5.Location = new System.Drawing.Point(726, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(270, 64);
+            this.groupBox5.TabIndex = 16;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Decrypt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 728);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.encryptedTextGroup);
+            this.Controls.Add(this.textGroup);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.textGroup.ResumeLayout(false);
+            this.textGroup.PerformLayout();
+            this.encryptedTextGroup.ResumeLayout(false);
+            this.encryptedTextGroup.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AdditionalInfo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button saveParamsButton;
+        private System.Windows.Forms.Button generateRSAButton;
+        private System.Windows.Forms.TextBox PlainText;
+        private System.Windows.Forms.GroupBox textGroup;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button loadPublicKey;
+        private System.Windows.Forms.Button EncryptText;
+        private System.Windows.Forms.GroupBox encryptedTextGroup;
+        private System.Windows.Forms.TextBox CipheredText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox CipheredBytes;
+        private System.Windows.Forms.Button SaveCipheredText;
+        private System.Windows.Forms.Button LoadBothKeys;
+        private System.Windows.Forms.Button DecryptFile;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ MySolution/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySolution
+{
+    public partial class Form1 : Form
+    {
+        RSACryptoServiceProvider rsa;
+
+        RSAParameters rsaParamsExcludePrivate;
+        RSAParameters rsaParamsIncludePrivate;
+
+        string publicPrivateKeyXML;
+        string publicOnlyKeyXML;
+        string decryptFileText;
+
+        OpenFileDialog openFileDialog;
+        SaveFileDialog saveFileDialog;
+
+        byte[] cipherbytes;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateRSAButton_Click(object sender, EventArgs e)
+        {
+            //UI
+            AdditionalInfo.Text = "";
+            PlainText.Text = "";
+            CipheredText.Text = "";
+            CipheredBytes.Text = "";
+
+            rsa = new RSACryptoServiceProvider();
+
+            rsaParamsIncludePrivate = rsa.ExportParameters(true);
+            rsaParamsExcludePrivate = rsa.ExportParameters(false);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("P: ");
+            for (int i = 0; i < rsaParamsIncludePrivate.P.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", rsaParamsIncludePrivate.P[i]));
+            }
+            sb.Append(Environment.NewLine);
+            sb.Append(Environment.NewLine);
+            sb.Append("Q: ");
+            for (int i = 0; i < rsaParamsIncludePrivate.Q.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", rsaParamsIncludePrivate.Q[i]));
+            }
+            sb.Append(Environment.NewLine);
+            sb.Append(Environment.NewLine);
+            sb.Append("K: ");
+            for (int i = 0; i < rsaParamsIncludePrivate.Exponent.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", rsaParamsIncludePrivate.Exponent[i]));
+            }
+            sb.Append(Environment.NewLine);
+            sb.Append(Environment.NewLine);
+            sb.Append("k: ");
+            for (int i = 0; i < rsaParamsIncludePrivate.D.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", rsaParamsIncludePrivate.D[i]));
+            }
+            AdditionalInfo.Text = sb.ToString();
+
+            //UI
+            saveParamsButton.Enabled = true;
+            loadPublicKey.Enabled = false;
+            LoadBothKeys.Enabled = false;
+        }
+
+        private void saveParamsButton_Click(object sender, EventArgs e)
+        {
+            publicPrivateKeyXML = rsa.ToXmlString(true);
+            publicOnlyKeyXML = rsa.ToXmlString(false);
+
+            saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            MessageBox.Show("Choose Place For Public Key Only.");
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string publicOnlyKeyXMLName = saveFileDialog.FileName;
+                System.IO.File.WriteAllText(publicOnlyKeyXMLName, publicOnlyKeyXML);
+
+                MessageBox.Show("Choose Place For Both Keys.");
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string publicPrivateKeyXMLName = saveFileDialog.FileName;
+                    System.IO.File.WriteAllText(publicPrivateKeyXMLName, publicPrivateKeyXML);
+                }
+                MessageBox.Show("Both Files Were Successfully Saved.");
+
+                //UI
+                loadPublicKey.Enabled = true;
+                LoadBothKeys.Enabled = true;
+            }
+        }
+
+        private void loadPublicKey_Click(object sender, EventArgs e)
+        {
+            //UI
+            AdditionalInfo.Text = "";
+            PlainText.Text = "";
+            CipheredText.Text = "";
+            CipheredBytes.Text = "";
+
+            rsa = new RSACryptoServiceProvider();
+            openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+
+            openFileDialog.Title = "Open Public Key File";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = openFileDialog.FileName;
+                    StreamReader reader = new StreamReader(fileName);
+                    publicOnlyKeyXML = reader.ReadToEnd();
+                    rsa.FromXmlString(publicOnlyKeyXML);
+                    AdditionalInfo.Text = publicOnlyKeyXML;
+
+                    //UI
+                    EncryptText.Enabled = true;
+                    generateRSAButton.Enabled = false;
+                    LoadBothKeys.Enabled = false;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void EncryptFile_Click(object sender, EventArgs e)
+        {
+            byte[] plainbutes = Encoding.UTF8.GetBytes(PlainText.Text);
+            cipherbytes = rsa.Encrypt(plainbutes, false);
+
+
+            CipheredText.Text = Encoding.UTF8.GetString(cipherbytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cipherbytes.Length; i++)
+            {
+                sb.Append(String.Format("{0,2:X2} ", cipherbytes[i]));
+            }
+            CipheredBytes.Text = sb.ToString();
+
+            //UI
+            SaveCipheredText.Enabled = true;
+        }
+
+        private void SaveCipheredText_Click(object sender, EventArgs e)
+        {
+            saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            MessageBox.Show("Choose Place For Ciphered Text.");
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string cipheredTextName = saveFileDialog.FileName;
+                System.IO.File.WriteAllText(cipheredTextName, CipheredText.Text);
+
+
+                //saveFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                MessageBox.Show("Choose Place For Ciphered Bytes.");
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string cipheredBytesName = saveFileDialog.FileName;
+                    System.IO.File.WriteAllBytes(cipheredBytesName, cipherbytes);
+                }
+
+                //UI
+                EncryptText.Enabled = false;
+                generateRSAButton.Enabled = true;
+                LoadBothKeys.Enabled = true;
+                SaveCipheredText.Enabled = false;
+            }
+        }
+
+        private void LoadBothKeys_Click(object sender, EventArgs e)
+        {
+            //UI
+            AdditionalInfo.Text = "";
+            PlainText.Text = "";
+            CipheredText.Text = "";
+            CipheredBytes.Text = "";
+
+            rsa = new RSACryptoServiceProvider();
+            openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+            openFileDialog.Filter = "XML files (*.xml)|*.xml";
+
+            MessageBox.Show("Choose XML File With Both Keys.");
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = openFileDialog.FileName;
+                    StreamReader reader = new StreamReader(fileName);
+                    publicPrivateKeyXML = reader.ReadToEnd();
+                    rsa.FromXmlString(publicPrivateKeyXML);
+                    AdditionalInfo.Text = publicPrivateKeyXML;
+
+
+                    MessageBox.Show("Choose File With Ciphered Bytes.");
+                    //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+                    openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            string bytesFileName = openFileDialog.FileName;
+                            cipherbytes = System.IO.File.ReadAllBytes(bytesFileName);
+
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = 0; i < cipherbytes.Length; i++)
+                            {
+                                sb.Append(String.Format("{0,2:X2} ", cipherbytes[i]));
+                            }
+                            CipheredBytes.Text = sb.ToString();
+
+                            //UI
+                            DecryptFile.Enabled = true;
+                            generateRSAButton.Enabled = false;
+                            loadPublicKey.Enabled = false;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void DecryptFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = @"C:\Users\Main\Desktop\Информационная безопасность\Лабы\Лр3 Asymmetric";
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            openFileDialog.Title = "Choose Ciphered Text File To Decrypt!";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileName = openFileDialog.FileName;
+                    StreamReader reader = new StreamReader(fileName);
+                    decryptFileText = reader.ReadToEnd();
+                    CipheredText.Text = decryptFileText;
+
+                    byte[] plainbutes = rsa.Decrypt(cipherbytes, false);
+
+                    PlainText.Text = Encoding.UTF8.GetString(plainbutes);
+
+                    //UI
+                    generateRSAButton.Enabled = true;
+                    loadPublicKey.Enabled = true;
+                    DecryptFile.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
